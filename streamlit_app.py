020746f39e2f3e1e6c2b512386b570f54817f449
Zendehrouh <f.zendehrouh@gmail.com>
2022-12-21 16:19:55 +0100

    Update streamlit_app.py
    add special menu header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
